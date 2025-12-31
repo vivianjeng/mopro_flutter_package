7c70b754f3d4289cbe6f1014b9a396b7341be370
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2025-12-31 09:39:48 +0800

    chore: restructure project with new bindings, remove deprecated files, and update configurations for multiple platforms

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,13 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-
+import 'dart:io';
 import 'package:flutter/services.dart';
-import 'package:mopro_flutter_package/mopro_flutter_package.dart';
-import 'package:mopro_flutter_package/mopro_flutter_types.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+import 'package:mopro_flutter_bindings/src/rust/third_party/mopro_example_app.dart';
+import 'package:mopro_flutter_bindings/src/rust/frb_generated.dart';
+
+Future<void> main() async {
+  await RustLib.init();
   runApp(const MyApp());
 }
 
@@ -16,11 +18,12 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  GenerateProofResult? _proofResult;
-  final _moproFlutterPlugin = MoproFlutterPackage();
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  CircomProofResult? _circomProofResult;
+  bool? _circomValid;
   bool isProving = false;
-  PlatformException? _error;
+  Exception? _error;
+  late TabController _tabController;
 
   // Controllers to handle user input
   final TextEditingController _controllerA = TextEditingController();
@@ -34,119 +37,191 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    // The inputs is a base64 string
-    var inputs = _proofResult?.inputs ?? "";
-    // The proof is a base64 string
-    var proof = _proofResult?.proof ?? "";
-    // Decode the proof and inputs to see the actual values
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
+  Widget buildCircomTab() {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (isProving) const CircularProgressIndicator(),
+          if (_error != null)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(_error.toString()),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _controllerA,
+              decoration: const InputDecoration(
+                labelText: "Public input `a`",
+                hintText: "For example, 5",
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _controllerB,
+              decoration: const InputDecoration(
+                labelText: "Private input `b`",
+                hintText: "For example, 3",
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: OutlinedButton(
+                  onPressed: () async {
+                    if (_controllerA.text.isEmpty ||
+                        _controllerB.text.isEmpty ||
+                        isProving) {
+                      return;
+                    }
+                    setState(() {
+                      _error = null;
+                      isProving = true;
+                    });
+
+                    FocusManager.instance.primaryFocus?.unfocus();
+                    CircomProofResult? proofResult;
+                    try {
+                      var inputs =
+                          '{"a":["${_controllerA.text}"],"b":["${_controllerB.text}"]}';
+                      final zkeyPath = await copyAssetToFileSystem(
+                        'assets/multiplier2_final.zkey',
+                      );
+                      proofResult = await generateCircomProof(
+                        zkeyPath: zkeyPath,
+                        circuitInputs: inputs,
+                        proofLib: ProofLib.arkworks,
+                      ); // DO NOT change the proofLib if you don't build for rapidsnark
+                    } on Exception catch (e) {
+                      print("Error: $e");
+                      proofResult = null;
+                      setState(() {
+                        _error = e;
+                      });
+                    }
+
+                    if (!mounted) return;
+
+                    setState(() {
+                      isProving = false;
+                      _circomProofResult = proofResult;
+                    });
+                  },
+                  child: const Text("Generate Proof"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: OutlinedButton(
+                  onPressed: () async {
+                    if (_controllerA.text.isEmpty ||
+                        _controllerB.text.isEmpty ||
+                        isProving) {
+                      return;
+                    }
+                    setState(() {
+                      _error = null;
+                      isProving = true;
+                    });
+
+                    FocusManager.instance.primaryFocus?.unfocus();
+                    bool? valid;
+                    try {
+                      var proofResult = _circomProofResult;
+                      final zkeyPath = await copyAssetToFileSystem(
+                        'assets/multiplier2_final.zkey',
+                      );
+                      valid = await verifyCircomProof(
+                        zkeyPath: zkeyPath,
+                        proofResult: proofResult!,
+                        proofLib: ProofLib.arkworks,
+                      ); // DO NOT change the proofLib if you don't build for rapidsnark
+                    } on Exception catch (e) {
+                      print("Error: $e");
+                      valid = false;
+                      setState(() {
+                        _error = e;
+                      });
+                    } on TypeError catch (e) {
+                      print("Error: $e");
+                      valid = false;
+                      setState(() {
+                        _error = Exception(e.toString());
+                      });
+                    }
+
+                    if (!mounted) return;
+
+                    setState(() {
+                      isProving = false;
+                      _circomValid = valid;
+                    });
+                  },
+                  child: const Text("Verify Proof"),
+                ),
+              ),
+            ],
+          ),
+          if (_circomProofResult != null)
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Proof is valid: ${_circomValid ?? false}'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Proof inputs: ${_circomProofResult?.inputs ?? ""}',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Proof: ${_circomProofResult?.proof ?? ""}'),
+                ),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text('Flutter App With MoPro')),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              if (isProving) const CircularProgressIndicator(),
-              if (_error != null)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(_error.toString()),
-                ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: _controllerA,
-                  decoration: const InputDecoration(
-                    labelText: "Public input `a`",
-                    hintText: "For example, 5",
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: _controllerB,
-                  decoration: const InputDecoration(
-                    labelText: "Private input `b`",
-                    hintText: "For example, 3",
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: OutlinedButton(
-                      onPressed: () async {
-                        if (_controllerA.text.isEmpty ||
-                            _controllerB.text.isEmpty ||
-                            isProving) {
-                          return;
-                        }
-                        setState(() {
-                          isProving = true;
-                        });
-
-                        FocusManager.instance.primaryFocus?.unfocus();
-                        GenerateProofResult? proofResult;
-                        PlatformException? error;
-                        // Platform messages may fail, so we use a try/catch PlatformException.
-                        // We also handle the message potentially returning null.
-                        try {
-                          var inputs =
-                              '{"a":["${_controllerA.text}"],"b":["${_controllerB.text}"]}';
-                          proofResult = await _moproFlutterPlugin.generateProof(
-                            "assets/multiplier2_final.zkey",
-                            inputs,
-                          );
-                          setState(() {
-                            isProving = false;
-                          });
-                        } on PlatformException catch (e) {
-                          print("Error: $e");
-                          error = e;
-                          proofResult = null;
-                        }
-
-                        // If the widget was removed from the tree while the asynchronous platform
-                        // message was in flight, we want to discard the reply rather than calling
-                        // setState to update our non-existent appearance.
-                        if (!mounted) return;
-
-                        setState(() {
-                          _proofResult = proofResult;
-                          _error = error;
-                        });
-                      },
-                      child: const Text("Generate Proof"),
-                    ),
-                  ),
-                ],
-              ),
-              if (_proofResult != null)
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text('Proof inputs: $inputs'),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text('Proof: $proof'),
-                    ),
-                  ],
-                ),
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text('Flutter App With MoPro'),
+        ),
+        body: ListView(
+          children: [buildCircomTab()],
         ),
       ),
     );
   }
+}
+
+/// Copies an asset to a file and returns the file path
+Future<String> copyAssetToFileSystem(String assetPath) async {
+  final byteData = await rootBundle.load(assetPath);
+  final directory = await getApplicationDocumentsDirectory();
+  final filename = assetPath.split('/').last;
+  final file = File('${directory.path}/$filename');
+  await file.writeAsBytes(byteData.buffer.asUint8List());
+  return file.path;
 }
